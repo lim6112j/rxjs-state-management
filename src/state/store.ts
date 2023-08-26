d2411fc50a03a9a504e6370920270ba4c2813131
ben lim <lim6112j@gmail.com>
2023-08-26 12:13:31 +0900

    commenting

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,11 @@
 import { Subject } from 'rxjs';
 import { Action, State } from '../types/types';
 import { combined_reducer } from '../reducer/reducer';
+/**
+ * singleton state store , reacting action state reducer
+ * @remarks this class mimics redux 
+ * 
+ */
 export class StateStore {
   private static instance: StateStore;
   private _action: Subject<Action>;
@@ -8,15 +13,29 @@ export class StateStore {
   private constructor() {
     this._action = new Subject<Action>();
   }
+  /**
+   * static method returns sole instance
+   * @param null
+   * @returns StateStore instance
+   */
   public static getInstance(): StateStore {
     if (!StateStore.instance) {
       StateStore.instance = new StateStore();
     }
     return StateStore.instance;
   }
+  /**
+ * triggering action
+ * @params {type: actiontype, paylod: unknown}
+ * @returns void
+ * @remarks subscriber triggering reducers for state change
+ */
   doAction(action: Action) {
     this._action.next(action)
   }
+  /**
+   * start listening action subject
+   */
   start() {
     this._action.subscribe(data => {
       combined_reducer.forEach(reducer => {
